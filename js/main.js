9b5ed1e00e8511829610b4ac56753c99ba1169b1
Ângela Igreja <angel.igreja@gmail.com>
2015-05-15 13:02:53 +0200

    Fix the navbar to the top once the user scrolls through the header.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function() {
+    "use strict"; // Start of use strict
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 900
+        }
+    })
+
+})(); // End of use strict
